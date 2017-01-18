ef3b9cc0472ed2823bfe872cb46197474ead18c4
Five52 <five52@laposte.net>
2017-01-18 17:08:12 +0100

    Create Rss classes

---
+++ app/src/main/java/fr/unicaen/info/dnr2i/rssapplication/RssItem.java
@@ -0,0 +1,35 @@
+public class RssItem {
+    int id;
+    String title;
+    String description;
+    String link;
+    String pubDate;
+
+    public RssItem(int id, String title, String description, String link, String pubDate) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.link = link;
+        this.pubDate = pubDate;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public String getLink() {
+        return this.link;
+    }
+
+    public String getPubDate() {
+        return this.pubDate;
+    }
+}

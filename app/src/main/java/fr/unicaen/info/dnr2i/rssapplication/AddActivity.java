afb26420cea883fdb0df9a345a812c6545d61c36
Thomas Fortin <contact@thomasfortin.fr>
2017-01-25 10:35:18 +0100

    Change menu to have one specific Menu per Activity and makes it work

--- app/src/main/java/fr/unicaen/info/dnr2i/rssapplication/AddActivity.java
+++ app/src/main/java/fr/unicaen/info/dnr2i/rssapplication/AddActivity.java
@@ -1,9 +1,11 @@
 package fr.unicaen.info.dnr2i.rssapplication;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -17,8 +19,17 @@ public class AddActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_to_list, menu);
         return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        if (menuItem.getItemId() == R.id.action_menu_list) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        return false;
     }
 
     public void addRssFeed(View view) {
--- app/src/main/java/fr/unicaen/info/dnr2i/rssapplication/MainActivity.java
+++ app/src/main/java/fr/unicaen/info/dnr2i/rssapplication/MainActivity.java
@@ -1,12 +1,15 @@
 package fr.unicaen.info.dnr2i.rssapplication;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -54,8 +57,19 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_to_add, menu);
         return true;
+    }
+
+
+
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        if (menuItem.getItemId() == R.id.action_menu_add) {
+            Intent intent = new Intent(this, AddActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        return false;
     }
 
     /**

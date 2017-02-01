31ffdc89f0c13bbc5ff3315b10fc7ede2e803835
plabadille <plabadille@gmail.com>
2017-02-01 16:12:52 +0100

    the delete feed button now delete the feed

--- app/src/main/java/fr/unicaen/info/dnr2i/rssapplication/MainActivity.java
+++ app/src/main/java/fr/unicaen/info/dnr2i/rssapplication/MainActivity.java
@@ -3,9 +3,12 @@ package fr.unicaen.info.dnr2i.rssapplication;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.ListView;
 
 import java.util.List;
@@ -62,5 +65,16 @@ public class MainActivity extends AppCompatActivity {
         }
         return titles;
     }
+
+/*    public void deleteFeed(View view) {
+        Log.d("test", "success");
+
+        ImageButton delButton = (ImageButton) view.findViewById(R.id.imgBtnEdit);
+        // Cache row position inside the button using `setTag`
+        delButton.get
+    }*/
+
+    /*bah t'as juste à créer la fonction dans la MainActivity et foutre ce qu'il faut dedans, en rajoutant bien le "onClick" sur le boutons
+    mais je crois que là différent y a une histoire de "OnItemClicked" ou je sais pas quoi sur la liste*/
 
 }
